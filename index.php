c4423e7c3dcc6f995b9da7c18304a6ee1b650e98
Hoang Trung <hoangtrungweb@gmail.com>
2017-09-21 09:49:39 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+	// if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+	// 	$uri = 'https://';
+	// } else {
+	// 	$uri = 'http://';
+	// }
+	// $uri .= $_SERVER['HTTP_HOST'];
+	// header('Location: '.$uri.'/dashboard/');
+	// exit;
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
+Something is wrong with the XAMPP installation :-(
